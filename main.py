f823a7f20b22eeec046fb8d7d2b11b6a3aa28eca
dpattinson <49662671+dpattinson@users.noreply.github.com>
2025-07-07 22:45:42 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import sys
+import os
+
+# Ensure the root directory is in the Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from PyQt6.QtWidgets import QApplication
 from app.main_window import MainWindow
 from app.controller import Controller
