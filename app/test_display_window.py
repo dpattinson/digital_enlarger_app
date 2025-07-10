eb55a0f90567a1da0db743a270768e439840b6e2
dpattinson <david.pattinson3@auspost.com.au>
2025-07-10 19:33:54 +1000

    single 8bit version of processed image is displayed

--- app/test_display_window.py
+++ app/test_display_window.py
@@ -158,6 +158,13 @@ class TestDisplayWindow(QWidget):
 
         painter = QPainter(padded)
         painter.drawImage(x_offset, y_offset, scaled)
+
+        from PyQt6.QtGui import QPen
+
+        pen = QPen(QColor(255, 0, 0))
+        pen.setWidth(3)
+        painter.setPen(pen)
+        painter.drawRect(0, 0, padded.width() - 1, padded.height() - 1)
         painter.end()
 
         return padded
