93337f191304fa89fbd8ff33b2e44157eeb4aee1
dpattinson <david.pattinson3@auspost.com.au>
2025-07-10 18:17:20 +1000

    single 8bit version of processed image is displayed

--- app/test_display_window.py
+++ app/test_display_window.py
@@ -1,5 +1,5 @@
 """Test mode display window for the Darkroom Enlarger Application."""
-import cv2
+import numpy as np
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QFrame
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import Qt, QTimer
